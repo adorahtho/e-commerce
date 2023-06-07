1d45af8b6127c4ac6dc397cd472e75e56584f92a
thoak90codes <adorahtho@gmail.com>
2023-06-06 22:39:21 -0500

    added product, category, product tag, and tag relationships to index.js file

--- models/index.js
+++ models/index.js
@@ -4,14 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+// Products belongsTo Category (one-to-one)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
+// Categories have many Products (one-to-many)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
+// Products belongToMany Tags (through ProductTag) (many-to-many)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
+// Tags belongToMany Products (through ProductTag) (many-to-many)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 module.exports = {
   Product,
   Category,
